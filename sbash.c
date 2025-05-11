ea2ec4f9cc50683a1e07cbd382a6ecae5b62f551
Meet Panchal <meet24086@gmail.com>
2025-05-11 20:39:50 +0530

    Change dir array in sbash.c

--- sbash.c
+++ sbash.c
@@ -152,7 +152,7 @@ int main(int argc, char *argv[])
 	char temp_command[250];
 	char black_command[25];
 	char danger_flags[][35]={"/bin/bash","/bin/sh","bin/bash","bin/sh","bash","sh","shell","-rf","-a","-al","-la","-f","--no-preserve-root","-R","alias","--norc","/dev/null","-exec","--overwrite"};
-    char danger_dir[][35]={".bashrc",".bash_profile",".ssh",".config",".bash_logout",".vimrc",".virc"};
+    char danger_dir[][35]={".bashrc",".profile",".bash_profile",".ssh",".config",".bash_logout",".vimrc",".virc","bin/"};
 	char *command;
 	char *dir;
     char *token;
